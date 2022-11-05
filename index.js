5df0dbf8abcfcbad8219e2a58799a38a148fc656
Igor de Castro <igordecastro07@gmail.com>
2022-11-05 12:17:33 -0300

    post tweets

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ app.use(cors())
 app.use(express.json())
 
 const serverUsers = []
-const tweets = []
+const tweets = [
+    {
+		username: "bobesponja",
+			avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+		  tweet: "eu amo o hub"
+	}
+]
 
 app.post("/sign-up", (req, res) => {
     const { username, avatar} = req.body
@@ -19,10 +25,22 @@ app.post("/sign-up", (req, res) => {
         avatar
     }
     serverUsers.push(newUser)
-    res.status(200).send("Ok!")
+    res.sendStatus(200)
     console.log(serverUsers)
 })
 
-
+app.post("/tweets", (req, res) => {
+    const { username, tweet} = req.body
+    if ( !username || !tweet ) {
+        return res.sendStatus(400)
+    }
+    const newTweet = {
+        username,
+        tweet
+    }
+    tweets.push(newTweet)
+    res.sendStatus(200)
+    console.log(serverUsers)
+})
 
 app.listen(5000, ()=> console.log("App running in port 5000"))
